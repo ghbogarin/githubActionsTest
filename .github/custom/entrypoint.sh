8671d681e66b0ce8f2f4eb7c8076d5060e32e886
Gaston <gastonb@qualabs.com>
2022-11-29 16:37:03 -0300

    test

---
+++ .github/custom/entrypoint.sh
@@ -0,0 +1,10 @@
+
+#!/bin/sh -l
+
+echo "------------------Firefox Version entrypoint-----------------------"
+echo ""
+firefox --version
+
+echo "------------------Geckodriver Versionn entrypoint-----------------------"
+echo ""
+firefox --version
