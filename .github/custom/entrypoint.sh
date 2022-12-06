eea02d078a1f7c5fea9a196256252fabab4fd463
Gaston <gastonb@qualabs.com>
2022-12-06 11:16:51 -0300

    test

--- .github/custom/entrypoint.sh
+++ .github/custom/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "------------------Firefox Version entrypoint-----------------------"
-echo ""
-firefox --version
-
-echo "------------------Geckodriver CAT-----------------------"
-echo ""
-cat etc/hosts
-
 echo "------------------Run Tests-----------------------"
 echo ""
 npm install -g npm
@@ -17,14 +9,6 @@ cd .github/custom/testProject
 echo "---------Installing Dependencies---------"
 echo ""
 npm i
-
-echo "------------------Geckodriver Version entrypoint-----------------------"
-echo ""
-geckodriver --version
-
-echo "------------------Geckodriver PID-----------------------"
-echo ""
-pidof geckodriver
 
 echo "---------Starting---------"
 echo ""
