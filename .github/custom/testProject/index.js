e498998c1a17e2ea5a939222f7fdd82bf1eda2e6
Gaston <gastonb@qualabs.com>
2022-12-07 12:16:17 -0300

    test

--- .github/custom/testProject/index.js
+++ .github/custom/testProject/index.js
@@ -1,20 +1,23 @@
 const {By, Key, until} = require('selenium-webdriver');
-const { Builder, Session } = require('selenium-webdriver');
+const { Builder, Session, logging } = require('selenium-webdriver');
 async function example() {
     let firefox = require('selenium-webdriver/firefox');
     const firefoxOptions = new firefox.Options()
     // console.log(firefoxOptions.useGeckoDriver(true));
     firefoxOptions.setBinary('/usr/bin/firefox')
     firefoxOptions.headless();
+    // const prefs = new logging.Preferences();
+    // prefs.setLevel(logging.Type.PERFORMANCE, logging.Level.ALL);
+    firefoxOptions.setLoggingPrefs()
     const driver = new Builder()
       .forBrowser('firefox')
       .setFirefoxOptions(firefoxOptions)
-    const sess = new Session('id_1',{})
-    console.log(sess.getCapabilities())
-    console.log(sess.getPageLoadStrategy())
-    console.log(sess.getFirefoxOptions())
-    console.log(sess.getHttpAgent())
-    console.log(sess.getServerUrl())
+    // const sess = new Session('id_1',{})
+    // console.log(sess.getCapabilities())
+    // console.log(sess.getPageLoadStrategy())
+    // console.log(sess.getFirefoxOptions())
+    // console.log(sess.getHttpAgent())
+    // console.log(sess.getServerUrl())
     driver.build()
   try {
     await driver.get('https://aulas.ort.edu.uy/login/index.php');
